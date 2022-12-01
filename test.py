c1362e4d2535a9722b7feef783ef4b440842d075
dfg-98 <dariofg98@gmail.com>
2022-12-01 16:19:06 -0500

    feat(tests): create output folder if does not exists and test using file substitution

--- test.py
+++ test.py
@@ -26,22 +26,34 @@ class TestCase:
         self.runned = False
         self.error = False
 
-    def run(self, logisim, circ):
-        result = ''
-        cmd = [logisim, circ, '-tty', 'halt,tty', '-load', self.file]
+    def run(self, logisim, circ, template):
+        result = ""
+        cmd = [
+            logisim,
+            template,
+            "-tty",
+            "halt,tty",
+            "-load",
+            self.file,
+            "-sub",
+            template,
+            circ,
+        ]
         try:
-            print_verbose(verbose_level_test_detail, 'Ejecutando el test: ', self.test_name)
+            print_verbose(
+                verbose_level_test_detail, "Ejecutando el test: ", self.test_name
+            )
             result = subprocess.run(cmd, stdout=subprocess.PIPE)
             self.runned = True
             if result.returncode != 0:
-                print('Error al ejecutar test: ', self.test_name)
+                print("Error al ejecutar test: ", self.test_name)
                 print(result.stdout)
                 print(result.stderr)
                 self.error = True
                 return
             self.result = bytes.decode(result.stdout[:-24]).strip()
         except subprocess.CalledProcessError as e:
-            print('Error al ejecutar test: ', self.test_name)
+            print("Error al ejecutar test: ", self.test_name)
             print(result.stdout)
             print(result.stderr)
             self.error = True
@@ -53,38 +65,49 @@ class TestCase:
 
         elif self.runned:
             status = self.result == self.expected_result
-            print('Test:', self.test_name,
-                  " ===============================================> ",  'OK' if status else 'FAIL')
-            print_verbose(verbose_level_test_detail, 'Esperado: ',
-                          self.expected_result, 'Obtenido: ', self.result)
+            print(
+                "Test:",
+                self.test_name,
+                " ===============================================> ",
+                "OK" if status else "FAIL",
+            )
+            print_verbose(
+                verbose_level_test_detail,
+                "Esperado: ",
+                self.expected_result,
+                "Obtenido: ",
+                self.result,
+            )
 
         else:
-            print('Test:', self.test_name, 'Debe correr el test antes')
+            print("Test:", self.test_name, "Debe correr el test antes")
 
-        print('---------------------------------------------------------------------------------------')
+        print(
+            "---------------------------------------------------------------------------------------"
+        )
 
 
 class TestSuite:
-
-    def __init__(self, dir, base_dir, circ):
+    def __init__(self, dir, base_dir, circ, template):
         self.base_dir = base_dir
         self.circ = circ
         self.path = dir
         self.test = []
+        self.template = template
         for file, path in self.searchAsmFiles():
             self.compile(file, path)
             expected = self.extractExpectedResult(path)
-            self.test.append(TestCase(file, os.path.join(self.base_dir, file, 'Bank'), expected))
+            self.test.append(
+                TestCase(file, os.path.join(self.base_dir, file, "Bank"), expected)
+            )
 
     def searchAsmFiles(self):
         for root, dirs, files in os.walk(self.path):
-            print_verbose(verbose_level_all,
-                          'Buscando archivos .asm en: ', root)
+            print_verbose(verbose_level_all, "Buscando archivos .asm en: ", root)
             for file in files:
                 try:
-                    if file.endswith('.asm'):
-                        print_verbose(verbose_level_all,
-                          'Archivo encontrado: ', file)
+                    if file.endswith(".asm"):
+                        print_verbose(verbose_level_all, "Archivo encontrado: ", file)
                         path = os.path.join(root, file)
                         yield file[:-4], path
                 except Exception as e:
@@ -92,52 +115,72 @@ class TestSuite:
 
     def compile(self, file, path):
         base_dir = os.path.join(self.base_dir, file)
-        print_verbose(verbose_level_all, 'Creando directorio: ', base_dir)
-        try: 
+        print_verbose(verbose_level_all, "Creando directorio: ", base_dir)
+        try:
             os.mkdir(base_dir)
         except FileExistsError as e:
-            print_verbose(verbose_level_all, 'Directorio existente: ', base_dir)
-        print_verbose(verbose_level_all, 'Compilando: ', path)
-        status = os.system(f'./assembler.py {path} -o {base_dir}')
-        if(status != 0):
-            print('Error al compilar: ', path)
+            print_verbose(verbose_level_all, "Directorio existente: ", base_dir)
+        print_verbose(verbose_level_all, "Compilando: ", path)
+        status = os.system(f"./assembler.py {path} -o {base_dir}")
+        if status != 0:
+            print("Error al compilar: ", path)
 
     def extractExpectedResult(self, path):
 
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             content = file.readlines()
 
         read = False
-        expected = ''
+        expected = ""
 
         for line in content:
-            if line.startswith('#prints'):
+            if line.startswith("#prints"):
                 expected = line[8:].strip()
                 break
-        print_verbose(verbose_level_all, 'Resultado esperado del test: ')
+        print_verbose(verbose_level_all, "Resultado esperado del test: ")
         print_verbose(verbose_level_all, expected)
         return expected
 
     def run_all(self):
         for test in self.test:
-            test.run('logisim', self.circ)
+            test.run("logisim", self.circ, self.template)
             test.print()
 
     def run_test(self, test_name):
         for test in self.test:
             if test.name == test_name:
-                test.run('logisim', self.circ)
+                test.run("logisim", self.circ, self.template)
                 test.print()
 
 
-if __name__ == '__main__':
-    usage = 'usage: %prog tests_dir circuit [options]'
+if __name__ == "__main__":
+    usage = "usage: %prog tests_dir circuit [options]"
 
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option('-o', '--out', dest='output_folder', type='string',
-                      default='.', help="Specify output folder to compile tests")
-    parser.add_option('-v', '--verbose', dest='verbose',
-                      type='int', help='Verbose debug mode')
+    parser.add_option(
+        "-o",
+        "--out",
+        dest="output_folder",
+        type="string",
+        default=".",
+        help="Specify output folder to compile tests",
+    )
+    parser.add_option(
+        "-t",
+        "--template",
+        dest="template",
+        type="string",
+        default="s-mips-template.circ",
+        help="The template .circ file without specific implementation",
+    )
+    parser.add_option(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        type="int",
+        default=0,
+        help="Verbose debug mode",
+    )
     options, args = parser.parse_args()
     if len(args) != 2:
         parser.error("Incorrect command line arguments")
@@ -146,8 +189,18 @@ if __name__ == '__main__':
     verbose_level = options.verbose
 
     output_folder = options.output_folder
+    template = options.template
     input_dir = args[0]
     circ = args[1]
 
-    test_suite = TestSuite(input_dir, output_folder, circ)
+    try:
+        os.mkdir(output_folder)
+    except FileExistsError as e:
+        print_verbose(verbose_level_all, "Directorio existente: ", output_folder)
+
+    if not os.path.exists(template):
+        print("El archivo de template no existe")
+        sys.exit(1)
+
+    test_suite = TestSuite(input_dir, output_folder, circ, template)
     test_suite.run_all()
